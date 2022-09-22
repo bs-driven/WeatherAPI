5c160474d0df82d7e707c86c9afb986773988fa0
Bradley <bradsamsess90@icloud.com>
2022-09-22 15:45:24 -0400

    api call start not working tho

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-var currentWeather = fetch ("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid= 1be94a81b76430455af4510e7c026f92");
+var currentWeather = fetch ("https://api.openweathermap.org/data/2.5/onecall?lat=&lon=&exclude=hourly,daily&appid=0586f93376fa6711704b9de4a46cfc56");
+// &appid=0586f93376fa6711704b9de4a46cfc56 (API key goes at the end)
+var desiredLocation = document.querySelector("#searchLocation");
+
+console.log(desiredLocation);
+
+fetch(currentWeather)
+.then(function (response) {
+    return response.json();
+  });
+  
+  
