0d187720e827010ecf467ef20d6228795a255279
Bradley <bradsamsess90@icloud.com>
2022-09-28 17:55:03 -0400

    script adds

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-var currentWeather = fetch ("https://api.openweathermap.org/data/2.5/onecall?lat=&lon=&exclude=hourly,daily&appid=0586f93376fa6711704b9de4a46cfc56");
-// &appid=0586f93376fa6711704b9de4a46cfc56 (API key goes at the end)
-var desiredLocation = document.querySelector("#searchLocation");
+// var currentWeather = fetch ("https://api.openweathermap.org/data/2.5/onecall?lat=&lon=&exclude=hourly,daily&appid=0586f93376fa6711704b9de4a46cfc56");
+// &appid=0586f93376fa6711704b9de4a46cfc56 (API key goes at the end) 
+var searchButton = document.querySelector("#searchBtn");
+// var searchHistory = JSON.parse(localstorage.getItem(""))
+// console.log(desiredLocation.value);
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
-console.log(desiredLocation);
+searchButton.addEventListener("click", search);
 
-fetch(currentWeather)
-.then(function (response) {
-    return response.json();
-  });
+function search (){
+  // var searchValue = desiredLocation.value;
+  var desiredLocation = document.querySelector("#searchLocation").value;
+
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${desiredLocation},&limit=4&appid=0586f93376fa6711704b9de4a46cfc56`)
+    .then(function (response) {
+        return response.json()
+      })
+      .then (function(data){
+        var city = data[0].name;
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        console.log(data);
+
+      console.log(city);
+      console.log(lat);
+      console.log(lon);
+      weatherData(lat, lon);
+      // console.log(country);
+
+      });  
+      // console.log()
   
-  
+}
+
+function weatherData(lat, lon){
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=3&appid=0586f93376fa6711704b9de4a46cfc56`)
+  .then(function (response) {
+    return response.json()
+  })
+  .then (function(data){
+    console.log(data)
+    var name = data.city.name;
+    console.log(name);
+    var weather = data.list[0].weather[0].main;
+    console.log(weather);
+    
+  }
+)}
